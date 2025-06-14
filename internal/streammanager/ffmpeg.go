c5574e5fe966b18b8f0fd6f3b555a2c0beffd6da
jbpratt <jbpratt78@gmail.com>
2025-06-14 03:15:15 -0500

    internal/streammanager: add flush_packets to ffmpeg stream args
    Signed-off-by: jbpratt <jbpratt78@gmail.com>

--- internal/streammanager/ffmpeg.go
+++ internal/streammanager/ffmpeg.go
@@ -102,7 +102,7 @@ func buildStreamingArgs(cfg ffmpegArgs) []string {
 	// Use stream copy for both video and audio since all processing is done in writeToFIFO
 	args = append(args, "-c", "copy")
 
-	args = append(args, "-f", "flv", "-flvflags", "no_duration_filesize", dest)
+	args = append(args, "-f", "flv", "-flvflags", "no_duration_filesize", "-flush_packets", "1", dest)
 	return args
 }
 
